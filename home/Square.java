b9e353bb158b60e7423e548d7467e7d1a1f5e0ab
KobaDev24 <106343806+KobaDev24@users.noreply.github.com>
2022-06-02 15:34:43 +0400

    Add files via upload

---
+++ home/Square.java
@@ -0,0 +1,16 @@
+package home;
+
+import ge.exercise.Rectangle;
+
+public final class Square extends Rectangle {
+    private int a;
+
+    public int gerArea(int x, int y){
+        return super.gerArea(x,y);
+    }
+
+    public int getPerimeter(int x, int y){
+        return super.getPerimeter(x,y);
+    }
+
+}
